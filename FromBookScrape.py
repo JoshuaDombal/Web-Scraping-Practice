9471c705a96349bba0706d01b13630f6690e3f50
JoshuaDombal <joshad123@hotmail.com>
2018-09-05 09:08:00 -0700

    Added error checking

--- FromBookScrape.py
+++ FromBookScrape.py
@@ -1,13 +1,25 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from requests import HTTPError
 
 
-try:
-    html = urlopen("https://www.bls.gov/news.release/empsit.t01.htm")
-    bsObj = BeautifulSoup(html.read(), features="lxml")
-    print(bsObj.body)
+url = "https://www.bls.gov/news.release/empsit.t01.htm"
 
-except HTTPError as e:
-    print(e)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+    try:
+        bsObj = BeautifulSoup(html.read(), features="lxml")
+        title = (bsObj.title)
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle(url)
+if title == None:
+    print("Title could not be found")
 else:
-    # Program continues
+    print(title)
